2bd69501e1eac541fc5bf1ea05ef91be414f3465
huijie.h <huijie@hudongpai.com>
2016-09-27 20:36:14 +0800

    增加编码判断模块

--- zh-crawl-app/venv/home.py
+++ zh-crawl-app/venv/home.py
@@ -1,14 +1,18 @@
+#!/usr/bin/python  
+#-*-coding:utf-8-*- 
+# import sys
+import chardet
 import requests 
 import bs4 
 
-rootUrl = 'https://www.zhihu.com'
-explore = rootUrl + '/explore'
+rootUrl = 'https://www.baidu.com/index.html' # http://blog.lanyus.com/archives/133.html 这个可以
+# explore = rootUrl + '/explore'
 
 def crawlPage(url):
 	req = requests.get(url)
-	# return req
-	root = bs4.BeautifulSoup(req.text)
-	return [a.attrs.get('href') for a in soup.select('div.feed-item')]
-	# return [a.attrs.get(href) for a in root.select('div.feed-item')]
+	txt = open(req.text) # reload # .decode('gbk')
+	return chardet.detect(req.text)
+	# root = bs4.BeautifulSoup(req.text)
+	# return txt
 
-print crawlPage(explore)
+print crawlPage(rootUrl)
