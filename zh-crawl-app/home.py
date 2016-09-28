00c76283f92ed8576a2df173044adae830bb642e
huijie.h <huijie@hudongpai.com>
2016-09-28 21:05:24 +0800

    备份今天的代码

--- zh-crawl-app/home.py
+++ zh-crawl-app/home.py
@@ -1,18 +1,3 @@
-# #!../../../../../Program Files/python/python  
-# #-*-coding:utf-8-*- 
-# # import sys
-# import chardet
-# import requests 
-# import bs4 
-# rootUrl = 'https://www.zhihu.com' # http://blog.lanyus.com/archives/133.html 这个可以
-# explore = rootUrl + '/explore'
-# def crawlPage(url):
-# 	req = requests.get(url)
-# 	root = bs4.BeautifulSoup(req.text, "html.parser")
-# 	print root
-# crawlPage(explore) 使用request和bs4还需要引入模板才可以哦
-
-
 import urllib2
 rootUrl = 'https://www.zhihu.com'
 explore = rootUrl + '/explore'
