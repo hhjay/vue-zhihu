27d13fba19b9af90d508e0362e490059a190e9b5
huijie.h <huijie@hudongpai.com>
2016-09-28 20:25:52 +0800

    使用urlopen直接抓取

--- zh-crawl-app/venv/home.py
+++ zh-crawl-app/venv/home.py
@@ -1,18 +1,23 @@
-#!/usr/bin/python  
-#-*-coding:utf-8-*- 
-# import sys
-import chardet
-import requests 
-import bs4 
+# #!../../../../../Program Files/python/python  
+# #-*-coding:utf-8-*- 
+# # import sys
+# import chardet
+# import requests 
+# import bs4 
 
-rootUrl = 'https://www.baidu.com/index.html' # http://blog.lanyus.com/archives/133.html 这个可以
+# rootUrl = 'https://www.zhihu.com' # http://blog.lanyus.com/archives/133.html 这个可以
 # explore = rootUrl + '/explore'
 
-def crawlPage(url):
-	req = requests.get(url)
-	txt = open(req.text) # reload # .decode('gbk')
-	return chardet.detect(req.text)
-	# root = bs4.BeautifulSoup(req.text)
-	# return txt
+# def crawlPage(url):
+# 	req = requests.get(url)
+# 	root = bs4.BeautifulSoup(req.text, "html.parser")
+# 	print root
 
-print crawlPage(rootUrl)
+# crawlPage(explore) 使用request和bs4还需要引入模板才可以哦
+import urllib2
+req = urllib2.Request('https://www.zhihu.com/explore')
+res = urllib2.urlopen(req)
+print res.read()
+
+
+## 明天尝试使用urlopen
