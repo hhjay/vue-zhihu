66c66b51f7396282098f0dea1fe258202a95bd05
sunhhjay@gamil.com <sunhhjay@qq.com>
2016-10-03 12:20:15 +0800

    增加vue-router 控制路由

--- zh-front-app/src/main.js
+++ zh-front-app/src/main.js
@@ -1,8 +1,10 @@
-import Vue from 'vue'
-import App from './app/home/modules'
+import Vue from 'vue';
+import App from './app/home/modules';
+import router from './router';
 
+router.star(App, 'body');
 /* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+// new Vue({
+//   el: 'body',
+//   components: { App }
+// })
---
+++ zh-front-app/src/router.js
@@ -0,0 +1,4 @@
+import Vue form 'vue';
+import VueRouter form 'vue-router';
+
+import home form 'app/home/';
