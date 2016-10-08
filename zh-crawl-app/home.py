bafc23c45778f6742438878480e9e34372e0a518
sunhhjay@gamil.com <sunhhjay@qq.com>
2016-10-08 19:51:13 +0800

    完善有无网络时的判断

--- zh-crawl-app/home.py
+++ zh-crawl-app/home.py
@@ -5,42 +5,43 @@ explore = rootUrl + '/explore'
 
 def crawlPage(url):
 	haveNet = crawlOut()
+	print haveNet
 	if haveNet == 000:
-		return False
+		fp = open("./home.html", "w")
+		txt = fp.readlines() # 读取所以内容
+		return txt
 	elif haveNet == 200:
 		res = urllib2.urlopen(url)
 		txt = res.read()
-		res.close
-		file = open("./buffer.html", "wb")
-		file.writelines(txt) # 写入多行在性能上比使用write写入更高
+		res.close # 关闭文档流并写入文件
+		fp = open("./home.html", "wb")
+		fp.writelines(txt) # 写入多行在性能上比使用write写入更高
 		return txt
-	else:
-		return False
 
 # 判断网络连接问题
 def crawlOut():
 	import urllib2
-	linkRes = []
+	# overRes = []
+	# overUrl = 'https://www.google.com.hk'
+	# try:
+	# 	s = urllib2.urlopen(overUrl)
+	# 	overRes.append(s)
+	# except:
+	# 	overRes.append(None)
+	# if not any(overRes):
+	# 	return 100 # 翻墙失败
+	# else:
+	overRes = []
 	testUrl = ['http://www.163.com/', 'http://www.qq.com/']
 	for i in testUrl:
 		try:
 			s = urllib2.urlopen(i)
-			linkRes.append(s)
+			overRes.append(s)
 		except:
-			linkRes.append(None)
-		if not any(linkRes):
-			return 000 # 联网失败
-		else:
-			overRes = []
-			overUrl = 'https://www.google.com.hk'
-			try:
-				s = urllib2.urlopen(overUrl)
-				overRes.append(s)
-			except:
-				overRes.append(None)
-			if not any(overRes):
-				return 100 # 翻墙失败
-			else:
-				return 200 # 翻墙失败 但联网成功
+			overRes.append(None)
+	if not any(overRes):
+		return 000 # 联网失败
+	else:
+		return 200 # 翻墙失败 但联网成功
 
 print crawlPage(rootUrl)
