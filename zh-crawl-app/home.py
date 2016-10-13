655f8d0065287f31cbcde9fb4c391a604172f0f7
sunhhjay@gamil.com <sunhhjay@qq.com>
2016-10-13 21:56:04 +0800

    注释含义

--- zh-crawl-app/home.py
+++ zh-crawl-app/home.py
@@ -54,3 +54,17 @@ h = crawlPage(rootUrl)
 m.feed(h)  
 m.close() 
 
+'''
+HTMLParser的成员函数: 
+ 
+    handle_startendtag  处理开始标签和结束标签 
+    handle_starttag     处理开始标签，比如<xx> 
+    handle_endtag       处理结束标签，比如</xx> 
+    handle_charref      处理特殊字符串，就是以&#开头的，一般是内码表示的字符 
+    handle_entityref    处理一些特殊字符，以&开头的，比如   
+    handle_data         处理数据，就是<xx>data</xx>中间的那些数据 
+    handle_comment      处理注释 
+    handle_decl         处理<!开头的，比如<!DOCTYPE html PUBLIC “-//W3C//DTD HTML 4.01 Transitional//EN” 
+    handle_pi           处理形如<?instruction>的东西 
+ 
+'''
